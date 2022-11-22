7ceb902a1187dac40d19b7334b25ec0e5fd30f3a
Wendi Zhang <wendizhang68@gmail.com>
2022-11-22 00:00:51 -0500

    date range can be updated now

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+const ctx = document.getElementById('myChart');
+
 var stockData=[]
 //stock data
 var timeData=[]
 let startDate = new Date("2020-05-30T23:50:21.817Z")
 let endDate = new Date("2023-05-30T23:50:21.817Z")
 //let interval = 730000
-let interval = 7300
-let deltaTime = Math.floor( ( endDate.getTime() - startDate.getTime() )/interval)
+let nData = 7300
+let deltaTime = Math.floor( ( endDate.getTime() - startDate.getTime() )/nData)
 console.log("deltaTime", deltaTime, startDate.getTime());
 let newDate=null
-for (var i = 1; i <= interval; i++) {
+for (var i = 1; i <= nData; i++) {
    stockData.push(Math.random()*10);
    newDate = new Date(startDate.getTime()+deltaTime*i);
    timeData.push(newDate);
@@ -26,22 +28,25 @@ function convertTZ(dates, tzString) {
   return res
 }
 
+
+
+
 function getData(timeSeries, stockSeries, start, end){
   let index_begin = -1, index_end=-1
   console.log("length: ", timeSeries.length)
   for (var i = 0; i< timeSeries.length; i++)
   {
-    let dt = new Date(timeSeries[i]);
+    // let dt = new Date(timeSeries[i]);
     // console.log(dt, start, timeSeries[i])
     // console.log(i,dt>start)
-    if (timeSeries[i]>start && index_begin === -1) 
+    if (timeSeries[i]>=start && index_begin === -1) 
     {
       index_begin = i
       console.log("begin",index_begin)
     }
     if (timeSeries[i]>end)
     {
-      index_end = i;
+      index_end = i
       console.log("end",index_end)
       break
     }
@@ -49,6 +54,65 @@ function getData(timeSeries, stockSeries, start, end){
   return [timeSeries.slice(index_begin,index_end), stockSeries.slice(index_begin,index_end)]
 }
 
+
+//creating chart
+let chart = null;
+let timezone="America/Chicago"
+timezone="UTC"
+function chartFunction(startDate,endDate, tz)
+{
+  let rangeStart=startDate
+  let rangeEnd=endDate
+  let newData=getData(timeData,stockData,rangeStart, rangeEnd)
+  let newTimeData=newData[0]
+  let newStockData=newData[1]
+
+
+  if (chart !== null)
+  {
+    chart.destroy()
+    
+  } 
+    chart = 
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: newTimeData,
+        datasets: [{
+          label:"stock",
+          data: newStockData,
+          borderColor: "rgba(220,20,20,1)",
+          backgroundColor: "rgba(220,20,20,0.5)",
+        }]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          x: {
+            maxBarThickness: 10,
+            type: "time",
+            time: {
+              unit: 'day',
+              timezone: tz,
+              tooltipFormat : 'YYYY-MM-DD HH:mm:SS ZZ'
+            },
+            ticks: {
+              autoSkip: true,
+              //maxTicksLimit: 10
+            },
+          },
+          y: {
+              ticks: {
+                beginAtZero: true
+              }
+          }
+        }
+      }
+    });
+
+}
 
 
 //date time picker
@@ -92,62 +156,13 @@ $(function(){
   },
   function(startDate,endDate, label)
   {
-    let rangeStart=startDate
-    let rangeEnd=endDate
-    let newData=getData(timeData,stockData,rangeStart, rangeEnd)
-    let newTimeData=newData[0]
-    let newStockData=newData[1]
+    console.log(startDate,endDate,label)
+    chartFunction(startDate,endDate, timezone)
 
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: convertTZ(newTimeData,timezone),
-        datasets: [{
-          label: 'stock',
-          data: newStockData,
-          borderWidth: 0.1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          },
-          x:{
-            ticks: {
-                autoSkip: true,
-                maxTicksLimit: 20
-            },
-            // type: 'time',
-            // time: {
-              // unit: 'hour',
-              // min: rangeStart,
-              // max: rangeEnd,
-              // displayFormats: {
-                  // hour: 'HH'
-              // },
-              // parser: function (utcMoment) {
-                  // return utcMoment.utcOffset('+0100');
-              // }                
-            // }
-          },
-        },
-      }
-    });
   }
 
 );
 });
-
-const ctx = document.getElementById('myChart');
-
-
-let rangeStart= new Date("2021-05-10T00:00:00.000Z")
-let rangeEnd= new Date("2021-06-10T00:00:00.000Z")
-let timezone="UTC"
-
-
-console.log(getData(timeData,stockData,rangeStart, rangeEnd))
 
 
 
